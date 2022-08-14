13bbb12b8e2fc768fd6e164e6700fadc848a93cb
ImHananaltaf <110372448+ImHananaltaf@users.noreply.github.com>
2022-08-14 21:57:15 +0500

    Create line function program.cpp

---
+++ line function program.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int  Line();
+int main(){
+	cout<<"I am Hanan Altaf\n";
+	cout<<"I am the student of UOK\n";
+	Line();
+}
+   Line()
+{
+	cout<<"I love Kashmir";
+}
