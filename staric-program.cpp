0eae32b600dea5663f44fc12b17c55d402f95fe5
ImHananaltaf <110372448+ImHananaltaf@users.noreply.github.com>
2022-08-14 23:22:55 +0500

    Create staric-program.cpp

---
+++ staric-program.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+void Line();
+int main()
+{
+	Line();
+	cout<<"HELLOOO";
+	Line();
+	cout<<"we are studying functions \n";
+	Line();
+}
+void Line()
+{
+	for(int a=1;a<=20;a++)
+	cout<<"*"<<endl;
+}
