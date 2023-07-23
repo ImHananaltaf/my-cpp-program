cd21f57448cee8b6c4f009d6f3fd3220026f6c3b
Alina Aasib <139845978+AlinaAasib@users.noreply.github.com>
2023-07-23 11:52:14 -0700

    Update Hello-world.cpp hanan

--- Hello-world.cpp
+++ Hello-world.cpp
@@ -1,6 +1,6 @@
 #include<iostream>
 using namespace std;
 int main(){
-	cout<<"This is the Hello World Program";
 	cout<<"This is the second line of hello world program";
+	cout<<"This is hello Word";
 }
